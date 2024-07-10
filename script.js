beb656796be4abead2a439ff632ca5f521a525e7
belo355 <edilson.belo@live.com>
2024-07-09 22:14:51 -0300

    update

--- script.js
+++ script.js
@@ -1,21 +1,29 @@
-const html = document.querySelector('html'); 
-const focoBt = document.querySelector('.app__card-button--foco'); 
-const curtoBt = document.querySelector('.app__card-button--curto'); 
-const longoBt = document.querySelector('.app__card-button--longo');
-const comecarBt = document.querySelector('#start-pause'); 
-const titulo = document.querySelector('.app__title'); 
-const timer = document.querySelector('#timer'); 
-const banner = document.querySelector('.app__image'); 
+const html = document.querySelector('html')
+const focoBt = document.querySelector('.app__card-button--foco')
+const curtoBt = document.querySelector('.app__card-button--curto')
+const longoBt = document.querySelector('.app__card-button--longo')
+const comecarBt = document.querySelector('#start-pause')
+const titulo = document.querySelector('.app__title')
+const timer = document.querySelector('#timer')
+const banner = document.querySelector('.app__image')
+const botoes = document.querySelectorAll('.app__card-button')
+const inputMusica = document.querySelector('#alternar-musica')
+const comercarBt = document.querySelector('#start-pause')
+const inicarOuPausar = document.querySelector('#start-pause span')
 
-const botoes = document.querySelectorAll('.app__card-button'); 
-
-const inputMusica = document.querySelector('#alternar-musica'); 
-const musica = new Audio('/sons/luna-rise-part-one.mp3'); 
+const musica = new Audio('/sons/luna-rise-part-one.mp3')
 const loopMusica = true; 
+const audioMusicPause = new Audio('/sons/pause.mp3') 
+const audioMusicStart = new Audio('/sons/play.wav') 
+const audioTempoFinalizado = new Audio('/sons/beep.mp3') 
 
 const duracaoFoco = 1500; 
 const duracaoDescansoCurto = 300; 
 const duracaoDescancoLongo = 900; 
+
+let tempoDecorridoEmSegundos = 5; 
+let intervalorId; 
+
 
 inputMusica.addEventListener('change', () => {
   if(musica.paused){
@@ -26,19 +34,19 @@ inputMusica.addEventListener('change', () => {
 })
 
 focoBt.addEventListener('click', () => {
-  alterarContexto('foco'); 
-  focoBt.classList.add('active'); 
+  alterarContexto('foco')
+  focoBt.classList.add('active')
 })
 
 
 curtoBt.addEventListener('click', () => {
-  alterarContexto('descanso-curto'); 
+  alterarContexto('descanso-curto')
   curtoBt.classList.add('active')
 })
 
 
 longoBt.addEventListener('click', () => {
-  alterarContexto('descanso-longo'); 
+  alterarContexto('descanso-longo')
   longoBt.classList.add('active')
 })
 
@@ -48,29 +56,70 @@ function alterarContexto(contexto) {
   }); 
 
   html.setAttribute('data-contexto', contexto); 
-  banner.setAttribute('src', `/images/${contexto}.png`); 
+  banner.setAttribute('src', `/images/${contexto}.png`)
+
   switch(contexto){
     case "foco": 
     titulo.innerHTML =  ` 
     Otimize sua produtividade,<br>
                 <strong class="app__title-strong">Mergulhe no que importa</strong>
     `
-    break; 
+    break
     case "descanso-curto": 
     titulo.innerHTML =  ` 
     Que tal dar uma respirada?<br>
                 <strong class="app__title-strong">Faça uma pausa curta.</strong>
     `
-    break;
+    break
     case "descanso-longo": 
     titulo.innerHTML =  ` 
     Hora de voltar a superficie.<br>
     <strong class="app__title-strong">Faça uma pausa longa.</strong>
     `
   default: 
-      break;
+      break
   }
 }
 
+const contagemRegressiva = () => {
+  if(tempoDecorridoEmSegundos <= 0){
+    //audioTempoFinalizado.play()
+    alert('tempo esgotado')
+    zerarTemporizador()
+    return
+  }
 
-  
+  tempoDecorridoEmSegundos -= 1;
+  console.log('Temporizador: ' + tempoDecorridoEmSegundos)
+  mostrarTempo()
+}
+
+comecarBt.addEventListener('click', iniciarOuPausarTemporizador)
+
+function iniciarOuPausarTemporizador() {
+  if(intervalorId){ //pause
+    zerarTemporizador() 
+    audioMusicPause.play()
+    return
+  }
+  audioMusicStart.play()
+  intervalorId = setInterval(contagemRegressiva, 1000) 
+  inicarOuPausar.setAttribute('src', '/images/pause.png')
+  inicarOuPausar.textContent = 'Pausar'
+}
+
+function zerarTemporizador(){
+  clearInterval(intervalorId)
+  inicarOuPausar.setAttribute('src', '/images/play_arrow.png')
+  inicarOuPausar.textContent = 'Começar'
+  intervalorId = null
+}
+
+function mostrarTempo(){
+  const tempo = tempoDecorridoEmSegundos
+  timer.innerHTML =  ` 
+        ${tempo}
+  `
+}
+
+mostrarTempo()
